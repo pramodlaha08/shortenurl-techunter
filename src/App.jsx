289502690ef8bb00e878be18ca14af0a4c7230c8
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-10-09 14:40:43 +0545

    feat: added my name

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import Hero from './components/Hero';
+import React from "react";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className='container mx-auto w-full px-4'>
-      <div className='mt-[10rem] flex justify-center items-center'>
-      <Hero />
+    <div className="flex flex-col min-h-screen">
+      <div className="container mx-auto w-full px-4 flex-grow">
+        <div className="mt-[10rem] flex justify-center items-center">
+          <Hero />
+        </div>
+      </div>
 
+      {/* Footer Text */}
+      <div className="text-center mb-4 text-gray-600">
+        <p className="text-sm">Made with ❤️ by Pramod</p>
       </div>
     </div>
   );
